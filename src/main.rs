bb98c0a370163938c4283e90238b5211ca51e6e7
minh25 <minh00012258@gmail.com>
2024-05-15 23:45:57 +0700

    .

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,40 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn hello_world() {
+    println!("Hello, world!");
+}
+
+// run on all entities with the Person and Name component
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Elaina Proctor" {
+            name.0 = "Elaina Hume".to_string();
+            break; // We don’t need to change any other names
+        }
+    }
+}
+
 fn main() {
-    App::new().run();
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .run();
 }
