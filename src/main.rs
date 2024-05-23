3fac32b10437c099ee56ce9ced1583a7aafed03b
minh25 <minh00012258@gmail.com>
2024-05-23 18:11:06 +0700

    draft

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
-use bevy::prelude::*;
+use std::io;
+use std::io::Write;
 
 fn main() {
-    App::new().run();
+    print!("Enter an array of numbers separated by spaces: ");
+    io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let numbers: Vec<i32> = input
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+    let mut sum = 0;
+    for number in numbers {
+        sum += number;
+    }
+    println!("The sum of the numbers is: {}", sum);
 }
